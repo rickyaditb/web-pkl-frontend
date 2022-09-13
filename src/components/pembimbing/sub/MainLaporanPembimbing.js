aab0572afb59e73424ca86d711bc5e9c1e96e84e
rickyaditb <rickyaditb@gmail.com>
2022-09-13 15:22:38 +0700

    feat: add status tab to pembimbing components

--- src/components/pembimbing/sub/MainLaporanPembimbing.js
+++ src/components/pembimbing/sub/MainLaporanPembimbing.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { Link } from 'react-router-dom';
 
 export default function MainLaporanPembimbing() {
-    const [laporan, setLaporan] = useState([]);
+    const [user, setUser] = useState([]);
 
     useEffect(() => {
         getDetailLaporan();
@@ -11,11 +11,48 @@ export default function MainLaporanPembimbing() {
 
     const getDetailLaporan = async () => {
         const response = await axios.get(`http://localhost:5000/laporan_detail`);
-        setLaporan(response.data);
+        setUser(response.data);
     };
+
+    const [tab, setTab] = useState('Aktif');
+
+    useEffect(() => {
+        let aktifContainer = [];
+        let nonContainer = [];
+        user.map((item, index) => {
+            if (item.status === "Aktif") {
+                aktifContainer.push({ ...item })
+            } else if (item.status === "Non Aktif") {
+                nonContainer.push({ ...item })
+            }
+        });
+        setAktif(aktifContainer);
+        setNonAktif(nonContainer);
+    }, [user]);
+
+    let [aktif, setAktif] = useState([]);
+    let [nonAktif, setNonAktif] = useState([]);
+
+    const switchToNon = () => {
+        setTab("Non")
+    }
+
+    const switchToAktif = () => {
+        setTab("Aktif")
+    }
 
     return (
         <div className="col-span-12 lg:col-span-10 mb-8">
+            <div class="text-lg font-bold text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700 bg-white shadow mb-3">
+                <ul class="flex flex-wrap -mb-px">
+                    <li class="mr-2 cursor-pointer" onClick={switchToAktif}>
+                        <p class={`inline-block p-4 rounded-t-lg border-b-2 ${tab === "Aktif" ? "text-blue-600 border-blue-600" : "border-transparent hover:text-gray-600 hover:border-gray-300"}`} aria-current="page">Staff Magang Aktif</p>
+                    </li>
+                    <li class="mr-2 cursor-pointer" onClick={switchToNon}>
+                        <p class={`inline-block p-4 rounded-t-lg border-b-2 ${tab === "Non" ? "text-blue-600 border-blue-600" : "border-transparent hover:text-gray-600 hover:border-gray-300"}`}>Staff Magang Non Aktif</p>
+                    </li>
+                </ul>
+            </div>
             <div className="bg-white p-3 shadow rounded mb-16">
                 <table className="text-left table-auto w-full">
                     <thead>
@@ -27,27 +64,50 @@ export default function MainLaporanPembimbing() {
                             <th className="font-semibold p-3 text-center">Aksi</th>
                         </tr>
                     </thead>
-                    <tbody>
-                        {laporan.map((item, index) => (
-                        <tr className="text-gray-900 border-t hover:bg-gray-100" key={item._id}>
-                            <td className="p-3">{index + 1}</td>
-                            <td className="p-3">{item.nama}</td>
-                            <td className="p-3">{item.asal_instansi}</td>
-                            <td className="p-3 text-center">{item.jumlah_laporan}</td>
-                            <td className="p-3 text-center">
-                                <div className="flex justify-center items-center">
-                                    <Link to={`/laporan_pembimbing/${item._id}`} className="p-2 font-semibold leading-tight text-blue-700 bg-blue-100 text-sm rounded flex justify-center items-center">
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
-                                            <path d="M12 15a3 3 0 100-6 3 3 0 000 6z" />
-                                            <path fillRule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 010-1.113zM17.25 12a5.25 5.25 0 11-10.5 0 5.25 5.25 0 0110.5 0z" clipRule="evenodd" />
-                                        </svg>
-                                        <p className="ml-1">Detail Laporan</p>
-                                    </Link>
-                                </div>
-                            </td>
-                        </tr>
-                        ))}
-                    </tbody>
+                    {tab === "Aktif" ?
+                        <tbody>
+                            {aktif.map((item, index) => (
+                                <tr className="text-gray-900 border-t hover:bg-gray-100" key={item._id}>
+                                    <td className="p-3">{index + 1}</td>
+                                    <td className="p-3">{item.nama}</td>
+                                    <td className="p-3">{item.asal_instansi}</td>
+                                    <td className="p-3 text-center">{item.jumlah_laporan}</td>
+                                    <td className="p-3 text-center">
+                                        <div className="flex justify-center items-center">
+                                            <Link to={`/laporan_pembimbing/${item._id}`} className="p-2 font-semibold leading-tight text-blue-700 bg-blue-100 text-sm rounded flex justify-center items-center">
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
+                                                    <path d="M12 15a3 3 0 100-6 3 3 0 000 6z" />
+                                                    <path fillRule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 010-1.113zM17.25 12a5.25 5.25 0 11-10.5 0 5.25 5.25 0 0110.5 0z" clipRule="evenodd" />
+                                                </svg>
+                                                <p className="ml-1">Detail Laporan</p>
+                                            </Link>
+                                        </div>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody> :
+                        <tbody>
+                            {nonAktif.map((item, index) => (
+                                <tr className="text-gray-900 border-t hover:bg-gray-100" key={item._id}>
+                                    <td className="p-3">{index + 1}</td>
+                                    <td className="p-3">{item.nama}</td>
+                                    <td className="p-3">{item.asal_instansi}</td>
+                                    <td className="p-3 text-center">{item.jumlah_laporan}</td>
+                                    <td className="p-3 text-center">
+                                        <div className="flex justify-center items-center">
+                                            <Link to={`/laporan_pembimbing/${item._id}`} className="p-2 font-semibold leading-tight text-blue-700 bg-blue-100 text-sm rounded flex justify-center items-center">
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
+                                                    <path d="M12 15a3 3 0 100-6 3 3 0 000 6z" />
+                                                    <path fillRule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 010-1.113zM17.25 12a5.25 5.25 0 11-10.5 0 5.25 5.25 0 0110.5 0z" clipRule="evenodd" />
+                                                </svg>
+                                                <p className="ml-1">Detail Laporan</p>
+                                            </Link>
+                                        </div>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    }
                 </table>
             </div>
         </div>
--- src/components/pembimbing/sub/MainPembimbing.js
+++ src/components/pembimbing/sub/MainPembimbing.js
@@ -5,7 +5,6 @@ import { Link } from 'react-router-dom';
 export default function MainPembimbing() {
     const [user, setUser] = useState([]);
 
-
     const auth = useContext(AuthContext);
 
     useEffect(() => {
@@ -21,38 +20,86 @@ export default function MainPembimbing() {
         setUser(response.data);
     };
 
+    const [tab, setTab] = useState('Aktif');
+
+    useEffect(() => {
+        let aktifContainer = [];
+        let nonContainer = [];
+        user.map((item, index) => {
+            if (item.status === "Aktif") {
+                aktifContainer.push({ ...item })
+            } else if (item.status === "Non Aktif") {
+                nonContainer.push({ ...item })
+            }
+        });
+        setAktif(aktifContainer);
+        setNonAktif(nonContainer);
+    }, [user]);
+
+    let [aktif, setAktif] = useState([]);
+    let [nonAktif, setNonAktif] = useState([]);
+
+    const switchToNon = () => {
+        setTab("Non")
+    }
+
+    const switchToAktif = () => {
+        setTab("Aktif")
+    }
 
     return (
         <div className="col-span-12 md:col-span-8 lg:col-span-7 transition duration-300 ease-in mb-1 md:mb-16">
             <div class="text-lg font-bold text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700 bg-white shadow mb-3">
                 <ul class="flex flex-wrap -mb-px">
-                    <li class="mr-2">
-                        <a href="#" class="inline-block p-4 text-blue-600 rounded-t-lg border-b-2 border-blue-600 active dark:text-blue-500 dark:border-blue-500" aria-current="page">Staff Magang Aktif</a>
+                    <li class="mr-2 cursor-pointer" onClick={switchToAktif}>
+                        <p class={`inline-block p-4 rounded-t-lg border-b-2 ${tab === "Aktif" ? "text-blue-600 border-blue-600" : "border-transparent hover:text-gray-600 hover:border-gray-300" }`} aria-current="page">Staff Magang Aktif</p>
                     </li>
-                    <li class="mr-2">
-                        <a href="#" class="inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">Staff Magang Non Aktif</a>
+                    <li class="mr-2 cursor-pointer" onClick={switchToNon}>
+                        <p class={`inline-block p-4 rounded-t-lg border-b-2 ${tab === "Non" ? "text-blue-600 border-blue-600" : "border-transparent hover:text-gray-600 hover:border-gray-300" }`}>Staff Magang Non Aktif</p>
                     </li>
                 </ul>
             </div>
-            <div className="flex flex-col gap-3">
-                {user.map((item, index) => (
-                    <Link to={`/profile/${item._id}`} key={item._id} className="bg-white p-5 rounded-lg shadow transform transition flex items-center">
-                        <img alt="foto-staff" src="https://randomuser.me/api/portraits/men/79.jpg" className="bg-gray-500 w-28 h-28 rounded-full mx-5" />
-                        <div className="ml-5">
-                            <div className="flex flex-col gap-1">
-                                <div>
-                                    <p className="text-gray-500">Nama</p>
-                                    <p className="font-bold text-gray-600 text-xl">{item.nama}</p>
-                                </div>
-                                <div>
-                                    <p className="text-gray-500">Asal Instansi</p>
-                                    <p className="font-bold text-gray-600 text-xl">{item.asal_instansi}</p>
+            {tab === "Aktif" ?
+                <div className="flex flex-col gap-3">
+                    {aktif.map((item, index) => (
+                        <Link to={`/profile/${item._id}`} key={item._id} className="bg-white p-5 rounded-lg shadow transform transition flex items-center">
+                            <img alt="foto-staff" src="https://randomuser.me/api/portraits/men/79.jpg" className="bg-gray-500 w-28 h-28 rounded-full mx-5" />
+                            <div className="ml-5">
+                                <div className="flex flex-col gap-1">
+                                    <div>
+                                        <p className="text-gray-500">Nama</p>
+                                        <p className="font-bold text-gray-600 text-xl">{item.nama}</p>
+                                    </div>
+                                    <div>
+                                        <p className="text-gray-500">Asal Instansi</p>
+                                        <p className="font-bold text-gray-600 text-xl">{item.asal_instansi}</p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </Link>
-                ))}
-            </div>
+                        </Link>
+                    ))}
+                </div>
+                :
+                <div className="flex flex-col gap-3">
+                    {nonAktif.map((item, index) => (
+                        <Link to={`/profile/${item._id}`} key={item._id} className="bg-white p-5 rounded-lg shadow transform transition flex items-center">
+                            <img alt="foto-staff" src="https://randomuser.me/api/portraits/men/79.jpg" className="bg-gray-500 w-28 h-28 rounded-full mx-5" />
+                            <div className="ml-5">
+                                <div className="flex flex-col gap-1">
+                                    <div>
+                                        <p className="text-gray-500">Nama</p>
+                                        <p className="font-bold text-gray-600 text-xl">{item.nama}</p>
+                                    </div>
+                                    <div>
+                                        <p className="text-gray-500">Asal Instansi</p>
+                                        <p className="font-bold text-gray-600 text-xl">{item.asal_instansi}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </Link>
+                    ))}
+                </div>
+            }
         </div>
     )
 }
