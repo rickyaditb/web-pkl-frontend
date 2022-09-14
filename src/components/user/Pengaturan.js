6165bb6758891f1faa5a05665392cab2dc435bd5
rickyaditb <rickyaditb@gmail.com>
2022-09-14 10:59:25 +0700

    feat: add change email and telephone number function

--- src/components/user/Pengaturan.js
+++ src/components/user/Pengaturan.js
@@ -1,19 +1,17 @@
-import React, { useState } from 'react'
-import Header from './sub/Header.js'
-import Sidebar from 'components/Sidebar.js'
-import Profile from 'components/user/sub/Profile.js'
-import MainPengaturan from './sub/MainPengaturan.js'
+import React from 'react';
+import Header from './sub/Header.js';
+import Sidebar from 'components/Sidebar.js';
+import Profile from 'components/user/sub/Profile.js';
+import MainPengaturan from './sub/MainPengaturan.js';
 import Bottombar from 'components/Bottombar.js';
 
 export default function Pengaturan() {
-    const [menu, setMenu] = useState("main");
-    
     return (
         <div>
             <Header />
             <div className="grid grid-cols-12 gap-3 mt-5">
                 <Sidebar activePage="pengaturan" />
-                <MainPengaturan menu={menu} setMenu={setMenu} />
+                <MainPengaturan />
                 <Profile kelas="hidden md:block" />
             </div>
             <Bottombar activePage="pengaturan" />
