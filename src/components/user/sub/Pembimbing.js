a076bc18ef3fa9d9107b41fdd8be9e290195754f
rickyaditb <rickyaditb@gmail.com>
2022-10-08 14:57:56 +0700

    refactor: remove unused message button

--- src/components/user/sub/Pembimbing.js
+++ src/components/user/sub/Pembimbing.js
@@ -46,16 +46,6 @@ export default function Pembimbing() {
                     <p className="text-gray-700 my-auto ml-2">{user.pembimbing.email}</p>
                 </div>
             </div>
-            <div className="bg-white text-gray-600 p-3 mt-3 font-bold rounded-lg shadow flex transform transition hover:bg-blue-100 duration-300 cursor-pointer">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12 my-auto mr-2 bg-blue-300 text-white p-2 rounded-lg" viewBox="0 0 20 20" fill="currentColor">
-                    <path d="M2 5a2 2 0 012-2h7a2 2 0 012 2v4a2 2 0 01-2 2H9l-3 3v-3H4a2 2 0 01-2-2V5z" />
-                    <path d="M15 7v2a4 4 0 01-4 4H9.828l-1.766 1.767c.28.149.599.233.938.233h2l3 3v-3h2a2 2 0 002-2V9a2 2 0 00-2-2h-1z" />
-                </svg>
-                <div className="border-l-2 pl-2 my-auto">
-                    <p className="text-xs text-gray-500">Punya Pertanyaan ?</p>
-                    <p className="text-lg -mb-1">Kirim Pesan</p>
-                </div>
-            </div>
         </motion.div>
     )
 }
