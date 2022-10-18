dedee1b9657135cfcb0a59e40d19adf7a3665d2d
rickyaditb <rickyaditb@gmail.com>
2022-10-18 14:32:35 +0700

    feat: improve button for mobile viewport

--- src/components/user/sub/MainLaporan.js
+++ src/components/user/sub/MainLaporan.js
@@ -111,29 +111,32 @@ export default function MainLaporan() {
                                         <td className="p-3">{moment(item.tanggal_laporan).format('dddd, Do MMMM YYYY')}</td>
                                         <td className="p-3">
                                             <div className="flex justify-center items-center">
-                                                <Link to={`/laporan/detail/${item._id}`} className="p-2 font-semibold leading-tight text-blue-700 bg-blue-100 text-sm rounded flex justify-center items-center">
+                                                <Link to={`/laporan/detail/${item._id}`} className="p-2 font-semibold leading-tight text-blue-700 bg-blue-100 text-sm rounded flex justify-center items-center text-center">
                                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
                                                         <path d="M12 15a3 3 0 100-6 3 3 0 000 6z" />
                                                         <path fillRule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 010-1.113zM17.25 12a5.25 5.25 0 11-10.5 0 5.25 5.25 0 0110.5 0z" clipRule="evenodd" />
                                                     </svg>
-                                                    <p className="ml-1">Lihat Laporan</p>
+                                                    <p className="hidden md:block ml-1">Lihat Laporan</p>
+                                                    <p className="block md:hidden ml-1">Lihat</p>
                                                 </Link>
                                             </div>
                                         </td>
                                         <td className="p-3">
                                             <div className="flex justify-center gap-2">
-                                                <Link to={`/laporan/edit/${item._id}`} className="p-2 font-semibold leading-tight text-yellow-700 bg-yellow-100 text-sm rounded flex justify-center items-center">
+                                                <Link to={`/laporan/edit/${item._id}`} className="p-2 font-semibold leading-tight text-yellow-700 bg-yellow-100 text-sm rounded flex justify-center items-center text-center">
                                                     <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
                                                         <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
                                                         <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" />
                                                     </svg>
-                                                    <p className="ml-1">Ubah Laporan</p>
+                                                    <p className="hidden md:block ml-1">Ubah Laporan</p>
+                                                    <p className="block md:hidden ml-1">Ubah</p>
                                                 </Link>
-                                                <button onClick={() => konfirmasi(item._id)} className="p-2 font-semibold leading-tight text-red-700 bg-red-100 text-sm rounded flex justify-center items-center">
+                                                <button onClick={() => konfirmasi(item._id)} className="p-2 font-semibold leading-tight text-red-700 bg-red-100 text-sm rounded flex justify-center items-center text-center">
                                                     <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
                                                         <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
                                                     </svg>
-                                                    <p className="ml-1">Hapus Laporan</p>
+                                                    <p className="hidden md:block ml-1">Hapus Laporan</p>
+                                                    <p className="block md:hidden ml-1">Hapus</p>
                                                 </button>
                                             </div>
                                         </td>
