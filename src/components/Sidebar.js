a699de15b51864def934fbb6305126591e96321b
rickyaditb <rickyaditb@gmail.com>
2022-10-20 08:13:00 +0700

    lang: change laporan to kegiatan

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -96,7 +96,7 @@ export default function Sidebar(props) {
                                 d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
                                 clip-rule="evenodd" />
                         </svg>
-                        <p className="ml-2">Laporan</p>
+                        <p className="ml-2">Kegiatan</p>
                     </motion.div> :
                     <div className={`text-gray-600 px-4 py-3 ${activePage === "laporan" && "bg-utama text-white "} font-bold rounded-lg flex cursor-pointer ${activePage !== "laporan" && "hover:bg-purple-300 "} duration-300 hover:bg-sekunder`}>
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 my-auto" viewBox="0 0 20 20"
@@ -105,7 +105,7 @@ export default function Sidebar(props) {
                                 d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
                                 clip-rule="evenodd" />
                         </svg>
-                        <p className="ml-2">Laporan</p>
+                        <p className="ml-2">Kegiatan</p>
                     </div>
                 }
             </Link>
